82cc5d7321fbd9759ef971ace6433a06260a91b2
Sudharsan Kamal <91054667+SudharsanKamal@users.noreply.github.com>
2023-07-19 18:50:57 +0530

    Initial commit

--- delete_column.py
+++ delete_column.py
@@ -1,27 +1,22 @@
 from google.cloud import bigtable
 
-project_name = 'prefab-botany-385705'
+project_name = 'project-id'
 instance_name = 'demobt'
 client = bigtable.Client(project=project_name, admin=True)
 instance = client.instance(instance_name)
 table = instance.table('employee')
 
-row_key = '3#HR'
+row_key = '7#HR'
 column_family_id = 'pers_info'
 
 row = table.row(row_key)
 row.delete_cell(column_family_id,"age")
 
-#To delete multiple cells/columns in a column family
-#row.delete_cells(column_family_id,["age","class"])
+#To delete multiple cells/columns in a column family 
 
+row_key = '11#HR'
+row = table.row(row_key)
+column_family_id = 'emp_info'
+row.delete_cells(column_family_id,["project","salary"])
 table.mutate_rows([row]) 
-
-# To delete columns in multiple rows
-
-# for row_key in range(2,7):
-#     row = table.row(str(row_key))
-#     row.delete_cell(column_family_id,"address")
-#     table.mutate_rows([row]) 
-
 
